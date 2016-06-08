2500515c1438d688060f9aa7107e59a3f27b1d15
DimaSanKiev <dmytro.burdyga@gmail.com>
2016-06-08 11:49:16 +0300

    add Handlebars template engine

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
 import static spark.Spark.get;
 
 public class Main {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
 
-        get("/", (req, res) -> "Welcome");
+        get("/", (req, res) -> {
+            return new ModelAndView(null, "index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
