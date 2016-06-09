2c3e1063864f4f7357aec34b38880d0d46797597
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-06-10 01:50:04 +0300

    add authentication with filters and attributes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,31 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
         staticFileLocation("/public");
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
+        before((req, res) -> {
+            if (req.attribute("username") == null) {
+                req.attribute("username", req.cookie("username"));
+            }
+        });
+
+        // authentication filter
+        before("/ideas", (req, res) -> {
+            // TODO - Send message about redirect...somehow
+            if (req.attribute("username") == null) {
+                res.redirect("/");
+                halt();
+            }
+        });
+
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
-            model.put("username", req.cookie("username"));
+            model.put("username", req.attribute("username"));
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
@@ -39,8 +52,7 @@ public class Main {
 
         post("/ideas", (req, res) -> {
             String title = req.queryParams("title");
-            // TODO: - This username is tied to the cookie implementation
-            CourseIdea courseIdea = new CourseIdea(title, req.cookie("username"));
+            CourseIdea courseIdea = new CourseIdea(title, req.attribute("username"));
             dao.add(courseIdea);
             res.redirect("/ideas");
             return null;
