8fcf987644cd042b5a2615eaa835e27ec04ed420
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-06-13 23:06:00 +0300

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,12 +19,11 @@ public class Main {
         CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
 
         before((req, res) -> {
-            if (req.attribute("username") == null) {
+            if (req.cookie("username") != null) {
                 req.attribute("username", req.cookie("username"));
             }
         });
 
-        // authentication filter
         before("/ideas", (req, res) -> {
             if (req.attribute("username") == null) {
                 setFlashMessage(req, "Whoops, please sign in first!");
@@ -35,6 +34,7 @@ public class Main {
 
         get("/", (req, res) -> {
             Map<String, String> model = new HashMap<>();
+            model.put("flashMessage", captureFlashMessage(req));
             model.put("username", req.attribute("username"));
             model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "index.hbs");
@@ -44,13 +44,13 @@ public class Main {
             Map<String, String> model = new HashMap<>();
             String username = req.queryParams("username");
             res.cookie("username", username);
-            model.put("username", username);
             res.redirect("/");
             return null;
-        }, new HandlebarsTemplateEngine());
+        });
 
         get("/ideas", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
+            model.put("flashMessage", captureFlashMessage(req));
             model.put("ideas", dao.findAll());
             model.put("flashMessage", captureFlashMessage(req));
             return new ModelAndView(model, "ideas.hbs");
@@ -58,7 +58,8 @@ public class Main {
 
         post("/ideas", (req, res) -> {
             String title = req.queryParams("title");
-            CourseIdea courseIdea = new CourseIdea(title, req.attribute("username"));
+            CourseIdea courseIdea = new CourseIdea(title,
+                    req.attribute("username"));
             dao.add(courseIdea);
             res.redirect("/ideas");
             return null;
@@ -67,7 +68,7 @@ public class Main {
         get("/ideas/:slug", (req, res) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("idea", dao.findBySlug(req.params("slug")));
-            return new ModelAndView(model, "idea.hdb");
+            return new ModelAndView(model, "idea.hbs");
         }, new HandlebarsTemplateEngine());
 
         post("/ideas/:slug/vote", (req, res) -> {
@@ -75,6 +76,8 @@ public class Main {
             boolean added = idea.addVoter(req.attribute("username"));
             if (added) {
                 setFlashMessage(req, "Thanks for your vote!");
+            } else {
+                setFlashMessage(req, "You already voted!");
             }
             res.redirect("/ideas");
             return null;
@@ -83,7 +86,8 @@ public class Main {
         exception(NotFoundException.class, (exc, req, res) -> {
             res.status(404);
             HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
-            String html = engine.render(new ModelAndView(null, "not-found.hbs"));
+            String html = engine.render(
+                    new ModelAndView(null, "not-found.hbs"));
             res.body(html);
         });
     }
@@ -106,8 +110,6 @@ public class Main {
         String message = getFlashMessage(req);
         if (message != null) {
             req.session().removeAttribute(FLASH_MESSAGE_KEY);
-        } else {
-            setFlashMessage(req, "You already voted!");
         }
         return message;
     }
